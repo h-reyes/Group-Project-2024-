4ce0cd5aa7ed2972020888b66a78e29b94e7815c
h-reyes <reyes.haley23@gmail.com>
2024-04-30 16:02:26 -0400

    1st group session

--- g_project/project.java
+++ g_project/project.java
@@ -1,7 +1,13 @@
 package g_project;
 
+import java.util.*;
 public class project{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int [] studentID = {220, 221, 222, 223, 224};
+        //int [] [] 
+String[] names = {" name 1", " name 2", " name 3", "name4", "name 5"};//{ad,am,ac}
+    String[] major ={"Computer Science","Nursing" ,"Math","Sociology","Child care"};   
+        
+    
     }
 }
